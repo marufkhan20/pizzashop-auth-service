13ba8cc8997139ab00063a876785131850854459
marufkhan20 <mdm326040@gmail.com>
2026-09-03 10:21:50 +0600

    feat: add email trimming validation and corresponding test case for user registration

--- tests/users/register.spec.ts
+++ tests/users/register.spec.ts
@@ -178,4 +178,26 @@ describe("POST /auth/register", () => {
       expect(users).toHaveLength(0);
     });
   });
+
+  describe("Fields are not in proper format", () => {
+    it("should trim the email field", async () => {
+      // Arrange
+      const userData = {
+        firstName: "Rashedul",
+        lastName: "Islam",
+        email: " marufkhan@gmail.com ",
+        password: "marufkhan",
+      };
+
+      // Act
+      await request(app).post("/auth/register").send(userData);
+
+      const userRepository = connection.getRepository(User);
+      const users = await userRepository.find();
+      const user = users[0] || { email: "" };
+
+      // Assert
+      expect(user.email).toBe("marufkhan@gmail.com");
+    });
+  });
 });
