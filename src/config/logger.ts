880f2cd4072f3841cdb75490173b7b7394eed38a
marufkhan20 <mdm326040@gmail.com>
2026-09-05 10:07:36 +0600

    refactor: simplify logger configuration by consolidating format definitions

--- src/config/logger.ts
+++ src/config/logger.ts
@@ -6,34 +6,27 @@ const logger = winston.createLogger({
   defaultMeta: {
     serviceName: "auth-service",
   },
+
+  format: winston.format.combine(
+    winston.format.timestamp(),
+    winston.format.json(),
+    winston.format.prettyPrint(),
+  ),
   transports: [
     new winston.transports.File({
       level: "info",
       dirname: "logs",
       filename: "combined.log",
-      format: winston.format.combine(
-        winston.format.timestamp(),
-        winston.format.json(),
-      ),
       silent: Config.NODE_ENV === "test",
     }),
     new winston.transports.File({
       level: "error",
       dirname: "logs",
       filename: "error.log",
-      format: winston.format.combine(
-        winston.format.timestamp(),
-        winston.format.json(),
-      ),
       silent: Config.NODE_ENV === "test",
     }),
     new winston.transports.Console({
       level: "info",
-      format: winston.format.combine(
-        winston.format.timestamp(),
-        winston.format.json(),
-        winston.format.prettyPrint(),
-      ),
     }),
   ],
 });
